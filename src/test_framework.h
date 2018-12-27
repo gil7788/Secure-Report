3696a3132d7b1689f6ad869c6fa4e4e86e9821f2
gil7788 <gil7788@gmail.com>
2018-12-27 19:20:32 +0200

    Add TODO comments to test_framework.h file

--- src/test_framework.h
+++ src/test_framework.h
@@ -10,9 +10,20 @@
 
 #include "matrix.h"
 
+
+/*
+ * Description: This framework tests the following flow:
+ *
+ * 1. Read an array and encrypt it.
+ * 2. Read a sketch matrix
+ * 4. Create a sketch of the indices where array equals query.  (encrypted)
+ * 5. Decrypt sketch.
+ * 6. Decode sketch.
+ * */
+
 template<class Number>
 class InputDatabase {
-// Mock class for db
+// Mock class for db, saves plain data but returns encrypted data
 private:
 	std::vector<int> _db;
 
@@ -35,10 +46,11 @@ inline void sketch(int out_size, Number *out, const InputDatabase<Number> &input
 
 	Matrix<unsigned int> m(out_size, input.size());
 
-    /*TODO
-     * 1. Generate sketch matrix with Saffron.py
-     * 2. Write to file result
-     * 3. Read from file
+    // TODO: 2. Read a sketch matrix
+    /*
+     * a. Generate sketch matrix with Saffron.py
+     * b. Write to file result
+     * c. Read from file
      * */
 
 	for (int i_out = 0; i_out < out_size; ++i_out)
@@ -56,6 +68,14 @@ inline void sketch(int out_size, Number *out, const InputDatabase<Number> &input
 
 template<class Number>
 bool test_sketch(int input_size, int sparsity) {
+    /* Description: Implement 1-6:
+     * 1. Read an array and encrypt it.
+     * 2. Read a sketch matrix
+     * 3. Create a sketch of the indices where array equals query.  (encrypted)
+     * 4. Decrypt sketch.
+     * 5. Decode sketch.
+     * */
+
     // Initialize X - input vector
 	std::vector<int> input(input_size);
 	for (int i_input = 0; i_input < input_size; ++i_input)
@@ -63,21 +83,25 @@ bool test_sketch(int input_size, int sparsity) {
 	for (int i_output = 0; i_output < sparsity; ++i_output)
 		input[rand() % input_size] = 1;
 
-    // Initialize db
+    // 1. Read an array and encrypt it.
 	InputDatabase<Number> input_db(input);
+    // Define encrypted result array
 	Number sketchEnc[sparsity];
+    // 3. Create a sketch of the indices where array equals query.  (encrypted)
 	sketch(sparsity, sketchEnc, input_db);
 
+    // 2. Read a sketch matrix, 4. Decrypt sketch.
+    // Decrypted result
 	int sketch[sparsity];
-
+    // Decrypt sketchEnc (which is encrypted result array)
 	for (int i_output = 0; i_output < sparsity; ++i_output)
 		sketch[i_output] = sketchEnc[i_output].to_int();
 
+    // TODO: 6. Decode sketch.
+    // decode sketch into decoded
 	std::vector<int> decoded(input_size);
 
-	// TODO:
-	// decode sketch into decoded
-
+    // Test if result is valid
 	for (int i_input = 0; i_input < input_size; ++i_input)
 		if (decoded[i_input] != input[i_input])
 			return false;
